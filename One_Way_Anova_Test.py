968c31d6c79a7c2fbddc6ac8cd8dd037c6550335
vanessailana <vanessa.klotzman.17@my.csun.edu>
2021-01-04 18:30:38 -0800

    Add files via upload

---
+++ One_Way_Anova_Test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from scipy.stats import f_oneway
+
+#call data frame
+#group1 dataframe1
+#group2 dataframe2
+#group3 dataframe 3
+#you can call as many dataframes as you like 
+f_oneway(group1, group2, group3)
+
