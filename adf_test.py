e4103673570d381aeb2501f6ec42c87f8e89d1e5
vanessailana <vanessa.klotzman.17@my.csun.edu>
2021-01-08 21:59:54 -0800

    Update adf_test.py

--- adf_test.py
+++ adf_test.py
@@ -2,8 +2,11 @@ from statsmodels.tsa.stattools import adfuller
 def adf_test(timeseries):
     #Perform Dickey-Fuller test:
     print ('Results of Dickey-Fuller Test:')
-    dftest = adfuller(timeseries, autolag='AIC')
-    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
-    for key,value in dftest[4].items():
-       dfoutput['Critical Value (%s)'%key] = value
+    result = adfuller(timeseries)
+    p_value=result[1]
+    print('ADF Statistic: %f' % result[0])
+    print('p-value: %f' % result[1])
+    print('Critical Values:')
+    for key, value in result[4].items():
+        print('\t%s: %.3f' % (key, value))
     print(f'Result: The series is {"not " if p_value > 0.05 else ""}stationary')
